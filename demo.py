2ed284e832a120dfd4c39bdb806e59168198f851
Morie <dorian.cormerais@gmail.com>
2023-12-08 12:25:36 +0100

    new commit

---
+++ demo.py
@@ -0,0 +1,17 @@
+# Déclaration de variables
+variable_int = 42
+variable_float = 3.14
+variable_str = "Hello, World!"
+variable_bool = True
+
+# Affichage de chaque variable
+print("Variable de type int:", variable_int)
+print("Variable de type float:", variable_float)
+print("Variable de type str:", variable_str)
+print("Variable de type bool:", variable_bool)
+
+# Affichage de la valeur de chaque variable avec son type associé sur la même ligne
+print("Variable de type int:", variable_int, "(", type(variable_int), ")")
+print("Variable de type float:", variable_float, "(", type(variable_float), ")")
+print("Variable de type str:", variable_str, "(", type(variable_str), ")")
+print("Variable de type bool:", variable_bool, "(", type(variable_bool), ")")
